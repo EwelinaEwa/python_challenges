92d1589c461ae98a8266dd070d2d05ffc7215472
EwelinaEwa <ewelina.kosciukiewicz@gmail.com>
2021-09-09 11:18:13 +0200

    Complete challenge 8 - Bob - WIP - umlauts giving error

--- python-bob/bob.py
+++ python-bob/bob.py
@@ -4,5 +4,13 @@
 
 
 def hey(what):
+    what_new = what.strip()
+    if what_new == "" or "\t" in what_new:
+        return "Fine. Be that way!"
+    if what_new[-1] == "!" and what_new[:3] != "Let" or what_new.isupper():
+        return "Whoa, chill out!"
+    if what_new[-1] == "?":
+        return "Sure."
 
-    return
+    else:
+        return "Whatever."
